3f679715d9c5c2405f70ef579ac2e79f42835736
DuzeyYT <59488004+DuzeyYT@users.noreply.github.com>
2022-03-28 16:04:31 +0200

    Update Comp.java

--- clickgui/comp/Comp.java
+++ clickgui/comp/Comp.java
@@ -11,24 +11,15 @@ public class Comp {
     public Module module;
     public Setting setting;
 
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {}
 
-    }
+    public void mouseReleased(int mouseX, int mouseY, int state) {}
 
-    public void mouseReleased(int mouseX, int mouseY, int state) {
-
-    }
-
-    public void drawScreen(int mouseX, int mouseY) {
-
-    }
+    public void drawScreen(int mouseX, int mouseY) {}
 
     public boolean isInside(int mouseX, int mouseY, double x, double y, double x2, double y2) {
         return (mouseX > x && mouseX < x2) && (mouseY > y && mouseY < y2);
     }
 
-    public void keyTyped(char typedChar, int keyCode) {
-
-    }
-
+    public void keyTyped(char typedChar, int keyCode) {}
 }
